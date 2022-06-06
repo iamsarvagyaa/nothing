244eb9b03fb670beb30f14d615a0c7d7b424f925
Sarvagya Sagar <iamsarvagyaa@gmail.com>
2022-06-07 01:02:43 +0530

    updates: added workflows

---
+++ scripts/fun.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2016
+
+# A simple script for fun w/wflows
+# <wflows> : production webhook flows
+
+OPTIONS=$1
+
+if [ -z "${OPTIONS}" ];
+then
+    printf 'Following are flags:\n'
+    printf '    -weather : current weather\n'
+fi
+
+if [ "${OPTIONS}" = "-weather" ];
+then
+    echo -e "$(curl -sL -X GET -G "https://<wflows>/weather" -H "User-Agent: curl/7.64.0")"
+fi
